75fab031ad69b5491cb98ba46c506eb25e6e304c
Diego Sousa <diego@lbnetwork.com.br>
2024-03-29 22:30:19 -0300

    Initial commit

---
+++ src/test/java/br/com/lbnetwork/syslibrary/SyslibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lbnetwork.syslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyslibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
