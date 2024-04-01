044c3cebb69ec374275f1e1b482af9845837f3bf
Diego Sousa <diego@lbnetwork.com.br>
2024-04-01 13:08:58 -0300

    DELETE method implemented Book Controller

--- src/main/java/br/com/lbnetwork/syslibrary/controllers/book/BookController.java
+++ src/main/java/br/com/lbnetwork/syslibrary/controllers/book/BookController.java
@@ -105,4 +105,76 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(bookModelOptional.get());
     }
 
+    @PutMapping("/books/{id}")
+    public ResponseEntity<?>  updateBook(@PathVariable(value = "id") UUID id,
+                                         @RequestBody @Valid BookRecordDto bookRecordDto){
+        Optional<BookModel> bookModelOptional = bookRepository.findById(id);
+        if (bookModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No category located in the database checks the UUID provided in the request." + id);
+        }
+        var bookModel = bookModelOptional.get();
+        var data = bookModelOptional.get().getCreatedAt();
+        BeanUtils.copyProperties(bookRecordDto, bookModel);
+        if (bookRecordDto.author() != null && bookRecordDto.author().getIdAuthor() != null){
+            UUID authorId = bookRecordDto.author().getIdAuthor();
+            Optional<AuthorModel> authorModelOptional = authorRepository.findById(authorId);
+            if (authorModelOptional.isPresent()){
+                bookModel.setAuthor(authorModelOptional.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Author not found with the given ID: " +authorId);
+            }
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Author ID not provided in DTO.");
+        }
+
+        if (bookRecordDto.category() != null && bookRecordDto.category().getIdCategory() != null){
+            UUID categoryId = bookRecordDto.category().getIdCategory();
+            Optional<CategoryModel> categoryModelOptional = categoryRepository.findById(categoryId);
+            if (categoryModelOptional.isPresent()){
+                bookModel.setCategory(categoryModelOptional.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Category not found with the given ID: " +categoryId);
+            }
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Category ID not provided in DTO.");
+        }
+
+        if (bookRecordDto.language() != null && bookRecordDto.language().getIdLanguage() != null){
+            UUID languageId = bookRecordDto.language().getIdLanguage();
+            Optional<LanguageModel> languageModelOptional = languageRepository.findById(languageId);
+            if (languageModelOptional.isPresent()){
+                bookModel.setLanguage(languageModelOptional.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Language not found with the given ID: " +languageId);
+            }
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Language ID not provided in DTO.");
+        }
+
+        if (bookRecordDto.publisher() != null && bookRecordDto.publisher().getIdPublisher() != null){
+            UUID publisherId = bookRecordDto.publisher().getIdPublisher();
+            Optional<PublisherModel> publisherModelOptional = publisherRepository.findById(publisherId);
+            if (publisherModelOptional.isPresent()){
+                bookModel.setPublisher(publisherModelOptional.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Publisher not found with the given ID: " +publisherId);
+            }
+        }else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Publisher ID not provided in DTO.");
+        }
+        bookModel.setCreatedAt(data);
+        bookModel.setUpdatedAt(new Date());
+        bookModel.setTitle(bookRecordDto.title().toUpperCase().trim());
+        return ResponseEntity.status(HttpStatus.OK).body(bookRepository.save(bookModel));
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<Object> deleteBook(@PathVariable(value = "id") UUID id){
+        Optional<BookModel> bookObject = bookRepository.findById(id);
+        if (bookObject.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Book located in the database checks the UUID provided in the request.");
+        }
+        bookRepository.delete(bookObject.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Book successfully deleted from the database.");
+    }
 }
