b81ab539aec1c074fe8f1a070c001ffef7f838a3
Diego Sousa <diego@lbnetwork.com.br>
2024-04-01 00:25:54 -0300

    findByTitle method implemented

--- src/main/java/br/com/lbnetwork/syslibrary/repositories/book/BookRepository.java
+++ src/main/java/br/com/lbnetwork/syslibrary/repositories/book/BookRepository.java
@@ -4,7 +4,9 @@ import br.com.lbnetwork.syslibrary.models.book.BookModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface BookRepository extends JpaRepository<BookModel, UUID> {
+    Optional<BookModel> findByTitle(String Title);
 }
