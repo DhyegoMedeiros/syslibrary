8b3a6e62e8424f724077c90eb025a38c84dd0035
Diego Sousa <diego@lbnetwork.com.br>
2024-03-31 21:20:54 -0300

    findByName method implemented

--- src/main/java/br/com/lbnetwork/syslibrary/repositories/book/LanguageRepository.java
+++ src/main/java/br/com/lbnetwork/syslibrary/repositories/book/LanguageRepository.java
@@ -1,10 +1,13 @@
 package br.com.lbnetwork.syslibrary.repositories.book;
 
+import br.com.lbnetwork.syslibrary.models.book.CategoryModel;
 import br.com.lbnetwork.syslibrary.models.book.LanguageModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface LanguageRepository extends JpaRepository<LanguageModel, UUID> {
+    Optional<LanguageModel> findByName(String Name);
 }
