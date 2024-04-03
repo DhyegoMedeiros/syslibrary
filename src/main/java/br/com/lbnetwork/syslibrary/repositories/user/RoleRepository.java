90fd795654d0b63eab7058da52070e5531a22306
Diego Sousa <diego@lbnetwork.com.br>
2024-04-02 23:29:15 -0300

    implemented search by role

--- src/main/java/br/com/lbnetwork/syslibrary/repositories/user/RoleRepository.java
+++ src/main/java/br/com/lbnetwork/syslibrary/repositories/user/RoleRepository.java
@@ -4,7 +4,9 @@ import br.com.lbnetwork.syslibrary.models.user.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface RoleRepository extends JpaRepository<RoleModel, UUID> {
+    Optional<RoleModel> findByRole(String role);
 }
