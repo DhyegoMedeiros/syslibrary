097d941339874646512f63bb2987ccf95b3f9dd7
Diego Sousa <diego@lbnetwork.com.br>
2024-04-02 23:28:52 -0300

    implemented search by name email phone

--- src/main/java/br/com/lbnetwork/syslibrary/repositories/user/UserRepository.java
+++ src/main/java/br/com/lbnetwork/syslibrary/repositories/user/UserRepository.java
@@ -4,8 +4,12 @@ import br.com.lbnetwork.syslibrary.models.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByUsername(String name);
+    Optional<UserModel> findByEmail(String email);
+    Optional<UserModel> findByPhone(String phone);
 }
